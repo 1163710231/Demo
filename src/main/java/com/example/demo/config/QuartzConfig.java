09af03ec74a11150c5923241b2df4168468cd893
4444 <570232054@qq.com>
2022-07-26 17:15:24 +0800

    Quartz SpringBoot 整合测试

---
+++ src/main/java/com/example/demo/config/QuartzConfig.java
@@ -0,0 +1,29 @@
+package com.example.demo.config;
+
+import com.example.demo.bootQuartz.QuartzJob1;
+import org.quartz.*;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class QuartzConfig {
+
+    @Bean
+    public JobDetail jobDetail1(){
+        return JobBuilder.newJob(QuartzJob1.class).storeDurably().build();
+    }
+
+    @Bean
+    public Trigger trigger1(){
+        SimpleScheduleBuilder scheduleBuilder = SimpleScheduleBuilder.simpleSchedule()
+                .withIntervalInSeconds(5) // 每一秒执行一次
+                .repeatForever(); // 永久重复，一直执行下去
+        return TriggerBuilder.newTrigger()
+                .forJob(jobDetail1())
+//                .withSchedule(scheduleBuilder)
+                .withSchedule(CronScheduleBuilder.cronSchedule("0/5 * * * * ?"))
+                .build();
+    }
+
+}
+
